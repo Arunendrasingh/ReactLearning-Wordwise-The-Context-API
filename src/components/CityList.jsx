2b24cb83492ec4090412a9aeeb2da9e765ca1f18
Arunendra Singh <singharunendra978@gmail.com>
2024-02-28 10:02:39 +0530

    ...

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,17 +1,22 @@
-import CityItem from './CityItem'
-import styles from './CityList.module.css'
-import Spinner from "./Spinner"
-import Message from "./Message"
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
 
-function CityList({cityList, isLoading}) {
-    if (isLoading) return <Spinner />
+function CityList({ cityList, isLoading }) {
+  if (isLoading) return <Spinner />;
 
-    if (!cityList.length) return <Message message="No City is present to display. Please add new city or reload the page."/>
+  if (!cityList.length)
     return (
-        <ul className={styles.cityList}>
-            {cityList.map((city)=> <CityItem city={city}/>)}
-        </ul>
-    )
+      <Message message="No City is present to display. Please add new city or reload the page." />
+    );
+  return (
+    <ul className={styles.cityList}>
+      {cityList.map((city) => (
+        <CityItem city={city} />
+      ))}
+    </ul>
+  );
 }
 
-export default CityList
+export default CityList;
